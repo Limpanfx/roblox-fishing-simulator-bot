3d1c211a77d5d1420d2ed0d93c4c7b7514509cc1
Limpanfx <145815319+Limpanfx@users.noreply.github.com>
2024-06-05 17:02:52 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ import time
 import keyboard
 import random
 import win32api, win32con
-import pydirectinput
 
 # Function to simulate a mouse click at given coordinates
 def click(x, y):
@@ -21,7 +20,7 @@ def check_air_bubbles_on_screen():
 	s = pyautogui.screenshot()
 	for x in range(770, 1160):
 		for y in range(350, 730):
-			colorcode = (68, 252, 234)  # Blue bubbles / Blaue Blasen
+			colorcode = (68, 252, 234)  # Blue bubbles
 			tempvar = False
 			for x2 in range(5):
 				if s.getpixel((x + x2, y)) == colorcode:
@@ -38,8 +37,13 @@ def fish_click():
 		click(960, 960)
 
 # Main loop to check for bubbles until 'q' is pressed
+last_bubble_time = time.time()  # Initialize the last bubble time
 while keyboard.is_pressed('q') == False:
 	if check_air_bubbles_on_screen() == True:
 		click(960, 540)
 		fish_click()
+		last_bubble_time = time.time()  # Update the last bubble time
+	elif time.time() - last_bubble_time > 20:  # If no bubbles for 20 seconds
+		click(960, 540)  # Throw the bait back into the water
+		last_bubble_time = time.time()  # Update the last bubble time
 	time.sleep(1)
