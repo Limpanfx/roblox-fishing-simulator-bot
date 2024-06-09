606a5d194e6a805447aa86f87b7967799fcef21d
Limpanfx <145815319+Limpanfx@users.noreply.github.com>
2024-06-09 22:31:48 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -6,8 +6,14 @@ import time
 import keyboard
 import random
 import win32api, win32con
+import autoit
+from pynput.keyboard import Controller, Key
 
-# Function to simulate a mouse click at given coordinates
+# Function to simulate a mouse click at given coordinates using AutoIt
+def autoit_click(x, y):
+    autoit.mouse_click('left', x, y, 1)
+
+# Function to simulate a mouse click at given coordinates using win32con
 def click(x, y):
 	win32api.SetCursorPos((x, y))
 	time.sleep(random.uniform(0.001, 0.005))
@@ -36,6 +42,34 @@ def fish_click():
 		time.sleep(0.47)
 		click(960, 960)
 
+def autoit_click(x, y):
+    autoit.mouse_click('left', x, y, 1)
+
+def sell_fish():
+    time.sleep(8)
+    keyboard.press('o')
+    time.sleep(0.4)
+    keyboard.release('o')
+    autoit_click(1263,1111)
+    time.sleep(2)
+    autoit_click(1094,379)
+    time.sleep(0.5)
+    autoit_click(1094,379)
+    time.sleep(1)
+    autoit_click(1244,468)
+    time.sleep(1)
+    autoit_click(1244,468)
+    time.sleep(0.9)
+    autoit_click(1244,468)
+    time.sleep(0.9)
+    autoit_click(1244,468)
+    time.sleep(1)
+    autoit_click(1482,395)
+    time.sleep(0.5)
+    keyboard.press('i')
+    time.sleep(0.4)
+    keyboard.release('i')
+
 fish_collected = 0
 last_bubble_time = time.time()  # Initialize the last bubble time
 while keyboard.is_pressed('q') == False:
@@ -46,7 +80,9 @@ while keyboard.is_pressed('q') == False:
 		fish_collected = fish_collected + 1
 		print("You've collected:", fish_collected, "fishes!")
 	elif time.time() - last_bubble_time > 20:  # If no bubbles for 20 seconds
+		sell_fish()
 		click(960, 540)  # Throw the bait back into the water
 		last_bubble_time = time.time()  # Update the last bubble time
 		fish_collected = fish_collected - 1
+		print("You've collected:", fish_collected, "fishes!")
 print("In total you've collected:", fish_collected, "fishes!")
